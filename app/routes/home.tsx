e3857a345e4587f375c0810321255e49ae7a3097
vanya-vb <vbozadzhieva29@gmail.com>
2025-10-20 19:04:28 +0300

    add authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,34 +1,46 @@
-import Navbar from "~/components/Navbar";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
+import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
 
 export function meta({ }: Route.MetaArgs) {
-  return [
-    { title: "Resumind" },
-    { name: "description", content: "Smart feedback for your dream job" },
-  ];
+    return [
+        { title: "Resumind" },
+        { name: "description", content: "Smart feedback for your dream job" },
+    ];
 }
 
 export default function Home() {
-  return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-      <Navbar />
-      <section className="main-section">
-        <div className="page-heading py-16">
-          <h1>Track Your Applications & Resume Ratings</h1>
-          <h2>Review your submissions and check AI-powered feedback.</h2>
-        </div>
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
 
-        {resumes.length > 0 && (
-          <div className="resumes-section">
-            {resumes.map((resume) => (
-              <ResumeCard key={resume.id} resume={resume} />
-            ))}
-          </div>
-        )}
+    useEffect(() => {
+        if (!auth.isAuthenticated) {
+            navigate('/auth?next=/');
+        }
+    }, [auth.isAuthenticated]);
 
-      </section>
-    </main>
-  )
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar />
+            <section className="main-section">
+                <div className="page-heading py-16">
+                    <h1>Track Your Applications & Resume Ratings</h1>
+                    <h2>Review your submissions and check AI-powered feedback.</h2>
+                </div>
+
+                {resumes.length > 0 && (
+                    <div className="resumes-section">
+                        {resumes.map((resume) => (
+                            <ResumeCard key={resume.id} resume={resume} />
+                        ))}
+                    </div>
+                )}
+
+            </section>
+        </main>
+    )
 }
